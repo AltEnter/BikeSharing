68477e80e39dd6dc57d7b8bf416b9909da2c9b34
Van Tian <vantian@live.com>
2017-02-18 01:17:36 +0800

    开屏

--- BikeSharing.Droid/Activities/MainActivity.cs
+++ BikeSharing.Droid/Activities/MainActivity.cs
@@ -5,7 +5,12 @@ using Android.Content.PM;
 
 namespace BikeSharing.Droid
 {
-    [Activity(Label = "BikeSharing.Droid", MainLauncher = true, Icon = "@drawable/icon",Theme ="@style/MainTheme",ScreenOrientation =ScreenOrientation.Portrait)]
+    [Activity(
+        Label             = "BikeSharing.Droid",
+        Icon              = "@drawable/icon",
+        Theme             = "@style/MainTheme",
+        ScreenOrientation = ScreenOrientation.Portrait
+        )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
---
+++ BikeSharing.Droid/Activities/SplashActivity.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace BikeSharing.Droid.Activities
+{
+    [Activity(
+        Label             = "SplashActivity",
+        Icon              ="@drawable/icon",
+        Theme             ="@style/Theme.Splash",
+        MainLauncher      =true,
+        NoHistory         =true,
+        ScreenOrientation =Android.Content.PM.ScreenOrientation.Portrait
+        )]
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            InvokeMainActivity();
+        }
+
+        private void InvokeMainActivity()
+        {
+            var mainActivityIntent = new Intent(this, typeof(MainActivity));
+            StartActivity(mainActivityIntent);
+        }
+    }
+}
