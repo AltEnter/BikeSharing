f2c7ff4bc997387a82bf7337269348913164bec1
Van Tian <vantian@live.com>
2017-02-17 12:47:03 +0800

    导航服务

---
+++ BikeSharing.Application/Services/NavigationService.cs
@@ -0,0 +1,48 @@
+﻿using BikeSharing.Application.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BikeSharing.Application.ViewModels;
+
+namespace BikeSharing.Application.Services
+{
+    public class NavigationService : INavigationService
+    {
+        public Task InitializeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task NavigateBackAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task NavigateToAsync<TViewModel>() where TViewModel : ViewModel
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task NavigateToAsync<TViewModel>(object parameter) where TViewModel : ViewModel
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task NavigateToAsync(Type viewModelType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task NavigateToAsync(Type viewModelType, object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveLastFromBackStackAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BikeSharing.Application/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,30 @@
+﻿using BikeSharing.Application.Services;
+using BikeSharing.Application.Services.Interfaces;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeSharing.Application.ViewModels
+{
+    public class ViewModelLocator
+    {
+        private readonly IUnityContainer _unityContainer;
+        public static ViewModelLocator Instance => new ViewModelLocator();
+
+        protected ViewModelLocator()
+        {
+            _unityContainer = new UnityContainer();
+
+            //服务
+            RegisterSingleton<INavigationService, NavigationService>();
+        }
+
+        private void RegisterSingleton<TInferface, T>() where T : TInferface
+        {
+            _unityContainer.RegisterType<TInferface, T>(new ContainerControlledLifetimeManager());
+        }
+    }
+}
