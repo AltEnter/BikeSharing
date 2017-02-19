6bbe48726c3beeb80aad962872ae660fd45629d8
Van Tian <vantian@live.com>
2017-02-19 11:48:11 +0800

    a little

--- BikeSharing/ViewModels/LoginViewModel.cs
+++ BikeSharing/ViewModels/LoginViewModel.cs
@@ -9,6 +9,23 @@ namespace BikeSharing.ViewModels
 {
     public class LoginViewModel:ViewModel
     {
+        private string _userName;
+        public string UserName
+        {
+            get => _userName;
+            set=>this.RaiseAndSetIfChanged(ref _userName,value);
+        }
+
+        private string _password;
+
+        public string Password
+        {
+            get => _password;
+            set => this.RaiseAndSetIfChanged(ref _password, value);
+        }
+
+
+
         public LoginViewModel(IScreen hostScreen):base(hostScreen)
         {
 
