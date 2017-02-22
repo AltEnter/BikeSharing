18172c6c805ffaed0e60d9dc9fd3116e51d24540
Van Tian <vantian@live.com>
2017-02-22 23:25:14 +0800

    fixed

--- BikeSharing/Models/User.cs
+++ BikeSharing/Models/User.cs
@@ -9,21 +9,7 @@ namespace BikeSharing.Models
 {
     public class User:ValidateableModel
     {
-        private string _userName;
-
-        public string UserName
-        {
-            get => _userName;
-            set => this.RaiseAndSetIfChanged(ref _userName, value);
-        }
-
-        private string _password;
-
-        public string Password
-        {
-            get => _password;
-            set => this.RaiseAndSetIfChanged(ref _password, value);
-        }
-
+        public string UserName { get; set; }
+        public string Password { get; set; }
     }
 }
--- BikeSharing/ViewModels/LoginViewModel.cs
+++ BikeSharing/ViewModels/LoginViewModel.cs
@@ -4,6 +4,7 @@ using BikeSharing.Models;
 using BikeSharing.Services.Interfaces;
 using ReactiveUI;
 using Splat;
+using Xamarin.Forms;
 
 namespace BikeSharing.ViewModels
 {
@@ -11,18 +12,13 @@ namespace BikeSharing.ViewModels
     {
         private IAuthenticationService _authenticationService;
 
-        private User _user;
-
-        public User User
-        {
-            get => _user;
-            set => this.RaiseAndSetIfChanged(ref _user, value);
-        }
+        public User User { get; set; }
 
         public ReactiveCommand Login { get; private set; }
 
         public LoginViewModel(IScreen hostScreen, IAuthenticationService authenticationService = null) : base(hostScreen)
         {
+            User = new User();
             _authenticationService = Locator.Current.GetService<IAuthenticationService>();
         }
     }
